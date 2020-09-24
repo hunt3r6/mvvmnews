96ccdcd61eee77be7d5ea08f3b0a3a71c16fd97b
hunt3r6 <faisalmahadi126@gmail.com>
2020-09-24 14:01:37 +0700

    initial commit

---
+++ app/src/main/java/co/id/bismalabs/mvvmnews/NewsActivity.kt
@@ -0,0 +1,12 @@
+package co.id.bismalabs.mvvmnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
